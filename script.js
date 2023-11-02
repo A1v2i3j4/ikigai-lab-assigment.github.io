04547976db54a1a2ed714d2366459a5f476aaef9
A1v2i3j4 <98036414+A1v2i3j4@users.noreply.github.com>
2023-11-02 19:28:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/* style.css */
+/* Add your custom CSS here */
+
+/* Example styles for the header and footer */
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
